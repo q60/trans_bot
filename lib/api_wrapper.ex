329a9c1b647b0b183f57c98e730232fd9de2f762
llathasa-veleth <llathasa@outlook.com>
2022-01-06 18:08:25 +0300

    Add module for API methods' wrappers

---
+++ lib/api_wrapper.ex
@@ -0,0 +1,40 @@
+defmodule APIWrapper do
+  @moduledoc """
+  Wrapper for VK API methods
+  """
+
+  alias BalalaikaBear, as: VK
+  require Logger
+
+  @doc """
+  Wrapper for messages.send
+  """
+  @spec send_message(integer(), any(), [message: String.t(), attachment: String.t(), sticker_id: String.t()]) :: :ok
+  def send_message(peer_id, token, options \\ []) do
+    defaults = [
+      message: "",
+      attachment: "",
+      sticker_id: ""
+    ]
+
+    options = Keyword.merge(defaults, options)
+
+    result =
+      VK.Messages.send(%{
+        peer_id: peer_id,
+        random_id: Enum.random(0..(2 ** 64)),
+        message: options[:message],
+        attachment: options[:attachment],
+        sticker_id: options[:sticker_id],
+        access_token: token
+      })
+
+    case result do
+      {:ok, _} ->
+        Logger.info("Successfully sent message to #{peer_id}")
+
+      {_, error} ->
+        Logger.error("Message was not sent: #{inspect(error)}")
+    end
+  end
+end
